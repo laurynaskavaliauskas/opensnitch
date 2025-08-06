1a6acf91d61a97acbf3f2d05b992867fa33a699e
Laurynas Kavaliauskas <lka@lidlabs.com>
2025-08-06 13:20:46 -0700

    - committed upstream changes to multithreaded package

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -3,7 +3,7 @@ package core
 // version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.6.9"
+	Version = "1.6.9-tp"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
